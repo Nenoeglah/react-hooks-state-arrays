e83d10e31f7ebeea02f692aeebd53581405fcec5
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-07-27 13:41:10 +0300

    completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,26 +1,49 @@
+
 import React, { useState } from "react";
-import { spicyFoods, getNewRandomSpicyFood } from "../data";
+// import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
-function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
 
-  function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
-  }
+const SpicyFoodList = () => {
+  const [spicyFoods, setSpicyFoods] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
-  ));
+  const addSpicyFood = () => {
+    const newFood = inputValue.trim();
+    if (newFood !== '') {
+      setSpicyFoods((prevFoods) => [...prevFoods, newFood]);
+      setInputValue(''); 
+    }
+  };
+
+  const deleteSpicyFood = (index) => {
+    const updatedFoods = [...spicyFoods];
+    updatedFoods.splice(index, 1);
+    setSpicyFoods(updatedFoods);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   return (
     <div>
-      <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter a new spicy food..."
+      />
+      <button onClick={addSpicyFood}>Add New Food</button>
+      <ul>
+        {spicyFoods.map((food, index) => (
+          <li key={index}>
+            {food}
+            <button onClick={() => deleteSpicyFood(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default SpicyFoodList;
